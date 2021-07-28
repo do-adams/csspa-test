b6ec982501aab47b0f169c3d76f42ccc6032ef9d
Damián Adams <damian.adams@rewardstyle.com>
2021-07-28 18:38:47 -0500

    Add missing footer MFE for some reason

---
+++ footer/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Footer,
+  },
+};
+</script>
