5877b62cf630cafc54a3693cc9dc239562b2f1bb
Damián Adams <damian.adams@rewardstyle.com>
2021-07-28 17:48:54 -0500

    Init Single SPA with 3 MFEs

---
+++ body/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Body />
+  </div>
+</template>
+
+<script>
+import Body from "./components/Body.vue";
+
+export default {
+  name: "App",
+  components: {
+    Body,
+  },
+};
+</script>
---
+++ nav/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Nav />
+  </div>
+</template>
+
+<script>
+import Nav from "./components/Nav.vue";
+
+export default {
+  name: "App",
+  components: {
+    Nav,
+  },
+};
+</script>
